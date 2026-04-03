752f7a6647409050bbba333ca260158422927fbf
xp8zp2z4h7-droid <keith.demma@att.net>
2026-04-03 14:56:48 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      stream: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
